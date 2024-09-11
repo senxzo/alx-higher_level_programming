739f6eb703808c1a6734df9a99f1ac8f34e9cd59
senxzo <innocentaudu2821@gmail.com>
2024-09-11 18:12:55 -0400

    Python conditionals

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+for i in range(10):
+    for j in range(i + 1, 10):
+        if i != j:
+            print("{:02d}, {:02d}".format(i, j), end=", " if i < 8 or j < 9 else "\n")
---
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def islower(c):
+    char = ord(c)
+    if 97 <= char <= 122:
+        return True
+    else:
+        return False
+        

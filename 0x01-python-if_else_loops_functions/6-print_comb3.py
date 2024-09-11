ca5e4175d7e61547d3ea1bfd7f0b82520ab0e7d9
senxzo <innocentaudu2821@gmail.com>
2024-09-11 18:53:37 -0400

    Python conditionals

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-for i in range(10):
-    for j in range(i + 1, 10):
-        if i != j:
-            print("{:02d}, {:02d}".format(i, j), end=", " if i < 8 or j < 9 else "\n")
+for x in range(10):
+    for y in range(x + 1, 10):
+        if x == 8 and y == 9:
+            print('89')
+        else:
+            print('{}{}, '.format(x, y), end='')
